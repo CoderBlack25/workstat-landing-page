af6b90f4dee4cd1d6665b545e6ce3cdad6bd1552
CoderBlack25 <Koderblac@gmail.com>
2026-03-15 10:22:48 +0100

    removed workstat hero image

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ import AboutUs from "@/components/sections/timestat/aboutUs";
 import BookDemo from "@/components/layouts/bookDemo";
 import Footer from "@/components/layouts/footer";
 import workstat from "@/public/icons/workstat-logo.svg";
-import anima from "@/public/images/anima.png";
+//import anima from "@/public/images/anima.png";
 
 const page = () => {
   return (
@@ -39,7 +39,7 @@ const page = () => {
         buttonTextColor="text-white"
         extraText={`Built for organizations that want clarity, control, and scale in
             workforce management.`}
-        heroImage={anima}
+        //heroImage={anima}
       />
       <SolutionSection />
       <PlatformOverview />
